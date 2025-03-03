821c258892d164b46ad5a615c8800d4e088a323f
Yukesh3004 <yukesh3004@gmail.com>
2025-03-03 18:13:45 +0530

    linkedlist addedd

---
+++ java_code/src/LinkedList/Singly_linkedlist/Reverse_linkedlist.java
@@ -0,0 +1,21 @@
+package LinkedList.Singly_linkedlist;
+
+import java.util.Stack;
+
+public class Reverse_linkedlist { //leetcode problem no:206
+//    public Node reverseList(Node head){
+//        Stack<Integer> stack = new Stack<>();
+//
+//        Node temp = head;
+//        while(temp != null){  //put the node values into the stack
+//            stack.push(temp.val);
+//            temp = temp.next;
+//        }
+//
+//    }
+
+    public static void main(String[] args){
+
+    }
+
+}
