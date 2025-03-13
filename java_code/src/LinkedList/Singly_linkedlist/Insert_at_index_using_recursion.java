6e628e91d1acc3b6963520c713ad8d3e9f92e4f0
Yukesh3004 <yukesh3004@gmail.com>
2025-03-13 20:05:44 +0530

    3/13/2025 problems updated

---
+++ java_code/src/LinkedList/Singly_linkedlist/Insert_at_index_using_recursion.java
@@ -0,0 +1,30 @@
+package LinkedList.Singly_linkedlist;
+
+public class Insert_at_index_using_recursion {
+
+    public static Singly_linkedlist.Node insertUsingRecursion(int index, int val, Singly_linkedlist.Node head){
+        if(index == 0){
+            Singly_linkedlist sl = new Singly_linkedlist();
+            Singly_linkedlist.Node node = sl.new Node(val,head);
+            sl.size++;
+            return node;
+        }
+
+        head.next = insertUsingRecursion(--index, val, head.next);
+
+        return head;
+    }
+
+    public static void main(String[] args) {
+        Singly_linkedlist sl = new Singly_linkedlist();
+        sl.insertAtFirst(10);
+        sl.insertAtFirst(20);
+        sl.insertAtFirst(30);
+        sl.insertAtFirst(40);
+        sl.display(sl.head);
+
+        Singly_linkedlist.Node insertNode = insertUsingRecursion(3,25, sl.head);
+        sl.display(insertNode);
+    }
+
+}
