9a476eea99a7a7c696968f45f433b4404db2c545
Yukesh3004 <yukesh3004@gmail.com>
2025-03-27 22:15:30 +0530

    3/27/25

--- java_code/src/LinkedList/Singly_linkedlist/BookStore_maintaining.java
+++ java_code/src/LinkedList/Singly_linkedlist/BookStore_maintaining.java
@@ -1,24 +1,31 @@
 package LinkedList.Singly_linkedlist;
 
-public class BookStore_maintaining { // interview question
+class Node{
+    Node next;
+    String title;
+    String author;
+    String genre;
 
-    public class Node{
-        Node next;
-        String title;
-        String author;
-        String genre;
-
-        public Node(String title, String author, String genre){
-            this.title = title;
-            this.author = author;
-            this.genre = genre;
-            this.next = null;
-        }
+    public Node(String title, String author, String genre){
+        this.title = title;
+        this.author = author;
+        this.genre = genre;
+        this.next = null;
     }
 
-    Node head = null;
+    public Node(Node next){
+        this.title = null;
+        this.author = null;
+        this.genre = null;
+        this.next = next;
+    }
+}
 
-    public void addBooks(String title, String author, String genre){
+public class BookStore_maintaining { // interview question
+
+    static Node head = null;
+
+    public static void addBooks(String title, String author, String genre){
         Node newBook = new Node(title, author, genre);
         if(head == null){
             head = newBook;
@@ -29,11 +36,42 @@ public class BookStore_maintaining { // interview question
     }
 
     public static Node partitionBooksByGenre(Node head, String genre){
+        Node current = head;
+        Node prev = new Node(head);
+        Node newHead = prev;
+        Node next = head.next;
 
+        while(current != null){
+            if(current.genre.equals(genre) && next.genre.equals(genre)){
+                current.next = next.next;
+                next.next = prev.next;
+                prev.next = next;
+            }
+            prev = prev.next;
+            current = current.next;
+            next = current.next;
+        }
+
+        return newHead.next;
+    }
+
+    public static void display(Node head){
+        Node temp = head;
+        while(temp != null){
+            System.out.print(temp.genre + " , ");
+            temp = temp.next;
+        }
     }
 
     public static void main(String[] args) {
+        addBooks("The God of Small Things","Arundhati Roy","Fiction");
+        addBooks("The God of Small Things","Arundhati Roy","Science");
+        addBooks("The God of Small Things","Arundhati Roy","Fiction");
+        addBooks("The God of Small Things","Arundhati Roy","Romance");
+        display(head);
 
+        Node result = partitionBooksByGenre(head, "Fiction");
+        display(result);
     }
 
 }
